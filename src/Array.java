4b3f4b67ec22674da3a746e3bcd5e3a53b20ef80
Stanley <stanleyyylau@gmail.com>
2019-10-15 22:49:52 +0800

    generic array

--- src/Array.java
+++ src/Array.java
@@ -1,10 +1,10 @@
-public class Array {
+public class Array<E> {
 
-    private int[] data;
+    private E[] data;
     private int size;
 
     public Array(int capacity) {
-        data = new int[capacity];
+        data = (E[])new Object[capacity];
         size = 0;
     }
 
@@ -25,7 +25,7 @@ public class Array {
     }
 
     // 向所有元素后添加一个新元素
-    public void addLast(int e){
+    public void addLast(E e){
         if(size == data.length)
             throw new IllegalArgumentException("AddLast fail, Array is full");
 //        data[size] = e;
@@ -35,7 +35,7 @@ public class Array {
     }
 
     // 在所有元素前添加一个新元素
-    public void addFirst(int e){
+    public void addFirst(E e){
         if(size == data.length)
             throw new IllegalArgumentException("AddLast fail, Array is full");
 //        for (int i = 0; i < size; i++) {
@@ -47,7 +47,7 @@ public class Array {
     }
 
     // 在index索引的位置插入一个新元素e
-    public void add(int index, int e){
+    public void add(int index, E e){
         if(size == data.length)
             throw new IllegalArgumentException("AddLast fail, Array is full");
         if(index < 0 || index > size)
@@ -61,17 +61,67 @@ public class Array {
     }
 
     // 获取index索引位置的元素
-    public int get(int index){
+    public E get(int index){
         if(index < 0 || index >= size)
             throw new IllegalArgumentException("Get failed. Index is illegal.");
         return data[index];
     }
 
     // 修改index索引位置的元素为e
-    public void set(int index, int e){
+    public void set(int index, E e){
         if(index < 0 || index >= size)
             throw new IllegalArgumentException("Set failed. Index is illegal.");
         data[index] = e;
+    }
+
+    // 查找数组中是否有元素e
+    public boolean contains(E e){
+        for (int i = 0; i < size; i++) {
+            if (data[i] == e) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 查找数组中元素e所在的索引，如果不存在元素e，则返回-1
+    public int find(E e){
+        for (int i = 0; i < size; i++) {
+            if (data[i] == e) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // 从数组中删除index位置的元素, 返回删除的元素
+    public E remove(int index){
+        if(index < 0 || index >= size)
+            throw new IllegalArgumentException("Delete failed. Index is illegal.");
+
+        E res = data[index];
+        for(int i = index; i < size - 1 ; i++) {
+            data[i] = data[i+1];
+        }
+        size--;
+        return res;
+    }
+
+    // 从数组中删除第一个元素, 返回删除的元素
+    public E removeFirst(){
+        return  remove(0);
+    }
+
+    // 从数组中删除最后一个元素, 返回删除的元素
+    public E removeLast(){
+        return remove(size-1);
+    }
+
+    // 从数组中删除元素e
+    public void removeElement(E e){
+        int index = find(e);
+        if (index != -1)
+            remove(index);
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,15 @@ public class Main {
 
         arr.addFirst(-1);
         System.out.println(arr);
+        // [-1, 0, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        arr.remove(2);
+        System.out.println(arr);
+
+        arr.removeElement(4);
+        System.out.println(arr);
+
+        arr.removeFirst();
+        System.out.println(arr);
     }
 }
